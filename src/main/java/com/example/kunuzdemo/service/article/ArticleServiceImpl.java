e09c39d6ca498d2df097b7934ab08c3a0606a15b
Saidazim <toirovsaidazim84@gmail.com>
2023-12-02 16:26:17 +0500

    write method getById (article)

--- src/main/java/com/example/kunuzdemo/service/article/ArticleServiceImpl.java
+++ src/main/java/com/example/kunuzdemo/service/article/ArticleServiceImpl.java
@@ -4,6 +4,7 @@ import com.example.kunuzdemo.dtos.request.ArticleCreateDto;
 import com.example.kunuzdemo.dtos.response.ArticleResponseDto;
 import com.example.kunuzdemo.dtos.response.RegionResponseDTO;
 import com.example.kunuzdemo.entity.*;
+import com.example.kunuzdemo.exceptions.DataNotFoundException;
 import com.example.kunuzdemo.repository.ArticleRepository;
 import com.example.kunuzdemo.service.category.CategoryService;
 import com.example.kunuzdemo.service.media.MediaService;
@@ -12,6 +13,9 @@ import com.example.kunuzdemo.service.user.UserService;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -23,6 +27,7 @@ public class ArticleServiceImpl implements ArticleService {
     private final CategoryService categoryService ;
     private final MediaService mediaService ;
     private final ArticleRepository articleRepository ;
+
 
     @Override
     public ArticleResponseDto create(ArticleCreateDto articleCreateDto) {
@@ -44,4 +49,19 @@ public class ArticleServiceImpl implements ArticleService {
         articleResponseDto.setRegion(regionResponseDTO);
         return articleResponseDto ;
     }
+    @Override
+    public ArticleResponseDto getById(UUID articleId) {
+        Optional<Article> articleById = articleRepository.getArticleById(articleId);
+
+        if (articleById.isEmpty()){
+            throw new DataNotFoundException("Article not found !");
+        }
+        else {
+            articleById.get().setViewCount(articleById.get().getViewCount()+1);
+            articleRepository.save(articleById.get());
+
+            return modelMapper.map(articleById.get(), ArticleResponseDto.class);
+        }
+    }
+
 }
