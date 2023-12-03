ad8128af9bebc31591f7708585211b4ec55bc32b
Saidazim <toirovsaidazim84@gmail.com>
2023-12-03 10:58:02 +0500

    write method changeArticleStatus

--- src/main/java/com/example/kunuzdemo/repository/ArticleRepository.java
+++ src/main/java/com/example/kunuzdemo/repository/ArticleRepository.java
@@ -1,16 +1,16 @@
 package com.example.kunuzdemo.repository;
 
-import com.example.kunuzdemo.dtos.response.ArticleResponseDto;
+
 import com.example.kunuzdemo.entity.Article;
 import com.example.kunuzdemo.enums.Language;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
+
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.List;
+
 import java.util.Optional;
 import java.util.UUID;
 
