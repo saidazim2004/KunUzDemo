de9a0ac2456b4d8d1b03fb622f8297a85842f36c
Saidazim <toirovsaidazim84@gmail.com>
2023-12-07 11:33:38 +0500

    write finish all method (User)

---
+++ src/main/java/com/example/kunuzdemo/controller/UserController.java
@@ -0,0 +1,119 @@
+package com.example.kunuzdemo.controller;
+
+import com.example.kunuzdemo.dtos.request.ChangeRoleDTO;
+import com.example.kunuzdemo.dtos.request.UserUpdateProfileDTO;
+import com.example.kunuzdemo.dtos.response.UserResponseDTO;
+import com.example.kunuzdemo.service.user.UserService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/user")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @PreAuthorize("hasAnyRole('ADMIN', 'SUPER_ADMIN')")
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponseDTO> getById(@PathVariable UUID id) {
+        return ResponseEntity.ok(userService.getById(id));
+    }
+
+
+    @PreAuthorize("hasAnyRole('ADMIN', 'SUPER_ADMIN')")
+    @GetMapping("/search-by-email")
+    public ResponseEntity<List<UserResponseDTO>> searchByEmail(
+            @RequestParam(required = false, defaultValue = "0") int page,
+            @RequestParam(required = false, defaultValue = "10") int size,
+            @RequestParam @Email String email
+    ) {
+        return ResponseEntity.ok(userService.searchByEmail(email, page, size));
+    }
+
+
+    @PreAuthorize("hasAnyRole('ADMIN', 'SUPER_ADMIN')")
+    @GetMapping
+    public ResponseEntity<List<UserResponseDTO>> getAll(
+            @RequestParam(required = false, defaultValue = "0") Integer page,
+            @RequestParam(required = false, defaultValue = "10") Integer size
+    ) {
+        return ResponseEntity.ok(userService.getAll(page, size));
+    }
+
+
+    @PreAuthorize("hasAnyRole('ADMIN', 'SUPER_ADMIN')")
+    @GetMapping("/filter-by-role")
+    public ResponseEntity<List<UserResponseDTO>> filterByRole(
+            @RequestParam(required = false, defaultValue = "0") Integer page,
+            @RequestParam(required = false, defaultValue = "10") Integer size,
+            @RequestParam String role
+    ) {
+        return ResponseEntity.ok(userService.filterByRole(page, size, role));
+    }
+
+
+    @PreAuthorize("hasAnyRole('SUPER_ADMIN', 'ADMIN')")
+    @GetMapping("/all-deleted")
+    public ResponseEntity<List<UserResponseDTO>> getAllDeleted(
+            @RequestParam(required = false, defaultValue = "0") Integer page,
+            @RequestParam(required = false, defaultValue = "10") Integer size
+    ) {
+        return ResponseEntity.ok(userService.getAllDeleted(page, size));
+    }
+
+
+    @PreAuthorize("hasAnyRole('SUPER_ADMIN')")
+    @PutMapping("/block/{userID}")
+    public ResponseEntity<String> blocByID(@PathVariable UUID userID) {
+        return ResponseEntity.ok(userService.blockByID(userID));
+    }
+
+
+    @PreAuthorize("hasAnyRole('SUPER_ADMIN')")
+    @PutMapping("/unblock/{userID}")
+    public ResponseEntity<String> unblockByID(@PathVariable UUID userID) {
+        return ResponseEntity.ok(userService.unblockByID(userID));
+    }
+
+
+    @PreAuthorize("hasAnyRole('SUPER_ADMIN')")
+    @PutMapping("/change-role")
+    public ResponseEntity<UserResponseDTO> changeRole(
+            @RequestBody @Valid ChangeRoleDTO role
+    ) {
+        return ResponseEntity.ok(userService.changeRole(role));
+    }
+
+
+    @PreAuthorize("hasAnyRole('SUPER_ADMIN')")
+    @PutMapping("/update-profile/{userID}")
+    public ResponseEntity<UserResponseDTO> updateProfile(
+            @PathVariable UUID userID,
+            @RequestBody @Valid UserUpdateProfileDTO dto
+    ) {
+        return ResponseEntity.ok(userService.updateProfile(userID, dto));
+    }
+
+
+    @PreAuthorize("hasAnyRole('SUPER_ADMIN')")
+    @DeleteMapping("/delete/{userID}")
+    ResponseEntity<String> deleteByID(@PathVariable UUID userID) {
+        return ResponseEntity.ok(userService.deleteByID(userID));
+    }
+
+
+    @PreAuthorize("hasAnyRole('SUPER_ADMIN')")
+
+    @DeleteMapping("/delete-selected")
+    public ResponseEntity<String> deleteSelected(@Valid @RequestParam List<UUID> userIDs) {
+        userService.deleteSelectedUsers(userIDs);
+        return ResponseEntity.ok("Successfully deleted!");
+    }
+}
--- src/main/java/com/example/kunuzdemo/service/user/UserServiceImpl.java
+++ src/main/java/com/example/kunuzdemo/service/user/UserServiceImpl.java
@@ -3,19 +3,34 @@ package com.example.kunuzdemo.service.user;
 import com.example.kunuzdemo.dtos.request.ChangeRoleDTO;
 import com.example.kunuzdemo.dtos.request.UserUpdateProfileDTO;
 import com.example.kunuzdemo.dtos.response.UserResponseDTO;
+import com.example.kunuzdemo.entity.Media;
 import com.example.kunuzdemo.entity.UserEntity;
+import com.example.kunuzdemo.enums.UserRole;
+import com.example.kunuzdemo.enums.UserStatus;
 import com.example.kunuzdemo.exceptions.DataNotFoundException;
+import com.example.kunuzdemo.repository.MediaRepository;
 import com.example.kunuzdemo.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+
+import static com.example.kunuzdemo.enums.UserStatus.ACTIVE;
+
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService{
     private final UserRepository userRepository ;
+    private final ModelMapper modelMapper ;
+    private final MediaRepository mediaRepository ;
+
     @Override
     public UserEntity getUserByID(UUID id) {
         return userRepository.getUserEntityById(id);
@@ -26,65 +41,121 @@ public class UserServiceImpl implements UserService{
         return null;
     }
 
-    @Override
-    public UserEntity getUserByEmail(String email) {
-
-        Optional<UserEntity> user = userRepository.findByEmail(email);
-        if(user.isEmpty()){
-            throw new DataNotFoundException("User not found with Email: " + email);
-        }
-        else {
-            return user.get();
-        }
-    }
 
     @Override
     public List<UserResponseDTO> searchByEmail(String email, Integer page, Integer size) {
-        return null;
+        Sort sort = Sort.by(Sort.Direction.ASC, "email");
+        Pageable pageable = PageRequest.of(page, size, sort);
+        List<UserEntity> users = userRepository.searchByEmail(email, pageable).getContent();
+        return modelMapper.map(users, new TypeToken<List<UserResponseDTO>>() {}.getType());
     }
+
 
     @Override
     public List<UserResponseDTO> getAll(Integer page, Integer size) {
-        return null;
+        Sort sort = Sort.by(Sort.Direction.ASC, "firstName");
+        Pageable pageable = PageRequest.of(page, size, sort);
+        List<UserEntity> users = userRepository.findAllUsers(pageable).getContent();
+        return modelMapper.map(users, new TypeToken<List<UserResponseDTO>>() {
+        }.getType());
     }
 
     @Override
     public List<UserResponseDTO> filterByRole(Integer page, Integer size, String role) {
-        return null;
+        Sort sort = Sort.by(Sort.Direction.ASC, "firstName");
+        Pageable pageable = PageRequest.of(page, size, sort);
+
+        try {
+            List<UserEntity> users = userRepository.filterByRole(UserRole.valueOf(role), pageable).getContent();
+            return modelMapper.map(users, new TypeToken<List<UserResponseDTO>>() {}.getType());
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("Enum type not valid: " + role);
+        }
     }
 
     @Override
     public List<UserResponseDTO> getAllDeleted(Integer page, Integer size) {
-        return null;
-    }
-
-    @Override
-    public String blockByID(UUID userId) {
-        return null;
-    }
-
-    @Override
-    public String unblockByID(UUID userId) {
-        return null;
+        Sort sort = Sort.by(Sort.Direction.ASC, "firstName");
+        Pageable pageable = PageRequest.of(page, size, sort);
+        List<UserEntity> users = userRepository.findAllDeleted(pageable).getContent();
+        return modelMapper.map(users, new TypeToken<List<UserResponseDTO>>() {
+        }.getType());
     }
 
     @Override
     public UserResponseDTO changeRole(ChangeRoleDTO roleDTO) {
-        return null;
+        UserEntity user = findByID(roleDTO.getUserId());
+
+        try {
+            user.setRole(UserRole.valueOf(roleDTO.getRole()));
+            return modelMapper.map(userRepository.save(user), UserResponseDTO.class);
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("Enum type not valid: " + roleDTO.getRole());
+        }
     }
 
     @Override
-    public UserResponseDTO updateProfile(UUID userId, UserUpdateProfileDTO dto) {
-        return null;
+    public UserResponseDTO updateProfile(UUID userID, UserUpdateProfileDTO dto) {
+        UserEntity user = findByID(userID);
+
+        if (dto.getMediaId() != null) {
+            Media media = mediaRepository.findById(dto.getMediaId()).orElseThrow(
+                    () -> new DataNotFoundException("Media not found with ID: " + dto.getMediaId()));
+            user.setMedia(media);
+        }
+
+        modelMapper.map(dto, user);
+        UserEntity updatedUser = userRepository.save(user);
+        return modelMapper.map(updatedUser, UserResponseDTO.class);
     }
 
     @Override
-    public String deleteByID(UUID userId) {
-        return null;
+    public UserEntity getUserByEmail(String email) {
+        return userRepository.findByEmail(email).orElseThrow(
+                () -> new DataNotFoundException("User not found with Email: " + email)
+        );
+    }
+
+    private UserEntity findByID(UUID id) {
+        return userRepository.getUserByID(id).orElseThrow(
+                () -> new DataNotFoundException("user not found with ID: " + id));
     }
 
     @Override
-    public void deleteSelectedUsers(List<UUID> userIds) {
+    public String blockByID(UUID userID) {
+        UserEntity user = findByID(userID);
+        if(user.getStatus().equals(UserStatus.BLOCKED)) {
+            return "User already blocked";
+        }
+        user.setStatus(UserStatus.BLOCKED);
+        userRepository.save(user);
+        return "User blocked";
+    }
 
+    @Override
+    public String unblockByID(UUID userID) {
+        UserEntity user = findByID(userID);
+        if(user.getStatus().equals(ACTIVE)) {
+            return "User already unblocked";
+        }
+        user.setStatus(ACTIVE);
+        userRepository.save(user);
+        return "User unblocked";
+    }
+
+
+    @Override
+    public String deleteByID(UUID userID) {
+        UserEntity user = findByID(userID);
+        user.setDeleted(true);
+        userRepository.save(user);
+        return "user deleted";
+    }
+
+    @Override
+    public void deleteSelectedUsers(List<UUID> userIDs) {
+        for (UUID userID : userIDs) {
+            deleteByID(userID);
+        }
     }
 }
